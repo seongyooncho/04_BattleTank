b224263b3e6c74c2655c2021fd569289e70896c0
seongyooncho <seongyoon.cho@ixdl.kr>
2017-08-29 12:19:36 +0900

    Add TankAIController

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,6 +2,20 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto Tank = GetControlledTank();
+	if (Tank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("We got tank: %s"), *Tank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("We got no tank."));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
