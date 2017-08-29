c7eafa373f4f2e344476abc3958551d0f889a152
seongyooncho <seongyoon.cho@ixdl.kr>
2017-08-29 14:35:05 +0900

    Override tick on PlayerController. Initialize AimTowardsCrosshair.

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -16,7 +16,10 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
-
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	ATank* GetControlledTank() const;
+	void AimTowardsCrosshair();
 };
