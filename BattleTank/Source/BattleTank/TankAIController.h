0dfc50b98061e311b2212300bce423b99a6ea12f
seongyooncho <seongyoon.cho@ixdl.kr>
2017-09-01 08:27:52 +0900

    Organize imports

--- BattleTank/Source/BattleTank/TankAIController.h
+++ BattleTank/Source/BattleTank/TankAIController.h
@@ -3,12 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Tank.h"
-#include "GameFramework/Actor.h"
-#include "Engine/World.h"
 #include "AIController.h"
 #include "TankAIController.generated.h"
 
+class ATank;
 /**
  * 
  */
