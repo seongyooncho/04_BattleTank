51fa20f9c4a1483a234ae37104e8509eeba860e5
seongyooncho <seongyoon.cho@ixdl.kr>
2017-09-04 08:54:09 +0900

    Add force to track on throttle

--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -18,4 +18,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
+	// Max force per track, in Newton
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000; // Assume 40 ton tank, and 1g acceleration
 };
